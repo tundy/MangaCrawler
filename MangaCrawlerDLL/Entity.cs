08fd5992ab3bbb45ea1d3ca5ee7e1ef0f95d2960
Tundy Matúš Tundér <tunder.matus@gmail.com>
2015-10-17 01:11:21 +0200

    Added Entity Class

---
+++ MangaCrawlerDLL/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MangaCrawlerDLL
+{
+    internal abstract class Entity
+    {
+        internal int ID { get; set; }
+        internal string URL { get; set; }
+        internal string Miniature { get; set; }
+        internal string Title { get; set; }
+
+        internal abstract string GetDirectory();
+
+        protected Entity(int id)
+        {
+            ID = id;
+        }
+    }
+}
