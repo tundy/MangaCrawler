783e6d0690eeaaac2eb7dd36b12b1664155227c2
Tundy Matúš Tundér <tunder.matus@gmail.com>
2015-10-17 01:12:07 +0200

    Added Page Class

---
+++ MangaCrawlerDLL/Page.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MangaCrawlerDLL
+{
+    internal class Page : Entity
+    {
+        internal override string GetDirectory()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Page(string url, int id) : this(url, id, string.Empty) { }
+        internal Page(string url, int id, string title) : base(id)
+        {
+            URL = url;
+            Title = title;
+        }
+    }
+}
