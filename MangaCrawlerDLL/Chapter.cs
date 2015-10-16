4662571eb93ec211dbb2cd735502d1bdd49cb1d6
Tundy Matúš Tundér <tunder.matus@gmail.com>
2015-10-17 01:11:57 +0200

    Added Chapter Class

---
+++ MangaCrawlerDLL/Chapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MangaCrawlerDLL
+{
+    internal class Chapter : Entity
+    {
+        internal override string GetDirectory()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Chapter(string url, int id, string title) : base(id)
+        {
+            URL = url;
+            Title = title;
+        }
+    }
+}
