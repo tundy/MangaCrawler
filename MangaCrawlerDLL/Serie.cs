0f8fe1edcf97344bc4ba4f3c8030cbde5ad08ecf
Tundy Matúš Tundér <tunder.matus@gmail.com>
2015-10-17 01:11:49 +0200

    Added Serie class

---
+++ MangaCrawlerDLL/Serie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MangaCrawlerDLL
+{
+    internal class Serie : Entity
+    {
+        internal override string GetDirectory()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Serie(string url, int id, string title) : base(id)
+        {
+            URL = url;
+            Title = title;
+        }
+    }
+}
